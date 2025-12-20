a1056837ad512abf0c0c6f864e320a32a521eca0
a2adib <a3dib.life@gmail.com>
2025-12-20 23:59:12 +0600

    Adding the private route

--- src/Pages/Dashboard/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import { AuthContext } from '../../Provider/AuthProvider';
+
 
 const AllUsers = () => {
+
+    const axiosSecure = useAxiosSecure();
+    const [users, setUsers] = useState([]);
+    const {user} = useContext(AuthContext); 
+
+    useEffect(() => {
+        if(!user)return;           
+        axiosSecure.get('/users')
+            .then(res => {
+                setUsers(res.data);
+            })
+            .catch(err => {
+                console.error('Error fetching users:', err);
+            });
+    }, [axiosSecure, user]);
+    
+    console.log(users);
+    
+    
     return (
         <div>
             All Users Page
