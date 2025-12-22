cd26fc8b0651f1f593686603d9ae4960ef5a3b9f
a2adib <a3dib.life@gmail.com>
2025-12-22 15:59:34 +0600

    fixing my-request page

--- src/Pages/Dashboard/MyRequest.jsx
+++ src/Pages/Dashboard/MyRequest.jsx
@@ -1,11 +1,92 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const MyRequest = () => {
-    return (
-        <div>
-            my request page
-        </div>
-    );
+  const [totalRequests, setTotalRequests] = useState(0);
+  const [myRequests, setMyRequests] = useState([]);
+  const [itemsPerPage, setItemPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const axiosSecure = useAxiosSecure();
+
+  useEffect(() => {
+    axiosSecure
+      .get(`/my-requests?page=${currentPage - 1}&limit=${itemsPerPage}`)
+      .then((res) => {
+        setMyRequests(res.data.request || []);
+        setTotalRequests(res.data.totalRequest);
+      })
+      .catch((err) => {
+        console.error("Error fetching my requests:", err);
+      });
+  }, [axiosSecure, currentPage, itemsPerPage]);
+
+  const numberofPages = Math.ceil(totalRequests / 10);
+
+  const pages = [...Array(numberofPages).keys()].map((e) => e + 1);
+  console.log(pages);
+  console.log("Rendering with myRequests:", myRequests);
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  const handleNextPage = () => {
+    if (currentPage < numberofPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Hospital Name</th>
+              <th>blood group</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+
+            {myRequests.map((request, index) => (
+              <tr key={request._id} >
+                <th>{index + 1}</th>
+                <td>{request.recipientName}</td>
+                <td>{request.hospitalName}</td>
+                <td>{request.bloodGroup}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <button 
+        onClick={handlePrevPage}
+        className="btn">Prev</button>
+
+        {pages.map((page) => (
+          <button
+            key={page}
+            className={`btn ${currentPage === page ? "btn-active" : ""}`}
+            onClick={() => setCurrentPage(page)}
+          >
+            {page}
+          </button>
+        ))}
+
+        <button
+        onClick={handlePrevPage} 
+        className="btn">Next</button>
+      </div>
+    </div>
+  );
 };
 
 export default MyRequest;
